368ae02f6c6e8f6d8d6787502052d29500c16f54
BlueHorn07 <bluehornblues@gmail.com>
2025-03-31 22:09:18 +0900

    chore: target="_blank" rel="noopener noreferrer"

--- pages/board/calendar/index.jsx
+++ pages/board/calendar/index.jsx
@@ -25,6 +25,7 @@ const AnnouncementPage = ({ calendarList }) => {
             'https://www.postech.ac.kr/kor/college-life/academic_schedule.do'
           }
           style={{ backgroundColor: 'rgb(200, 1, 80)', color: 'white' }}
+          target="_blank" rel="noopener noreferrer"
         >
           공식 학사일정 확인하기
         </Button>
