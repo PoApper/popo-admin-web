dfd15cd5b062d780e4987b49c31632ed751030e4
Gwanho Kim <khkim6040@gmail.com>
2025-09-13 22:22:01 +0900

    format

--- utils/axios.instance.js
+++ utils/axios.instance.js
@@ -3,7 +3,9 @@ import axios from 'axios';
 const next_env = process.env.NEXT_PUBLIC_ENV;
 
 if (!next_env) {
-  throw new Error('.env 파일에 NEXT_PUBLIC_ENV 환경변수가 설정되어 있지 않습니다.');
+  throw new Error(
+    '.env 파일에 NEXT_PUBLIC_ENV 환경변수가 설정되어 있지 않습니다.',
+  );
 }
 
 export const popoApiUrl =
@@ -14,7 +16,9 @@ export const popoApiUrl =
       : next_env === 'local'
         ? 'https://localhost:4000'
         : (() => {
-            throw new Error(`NEXT_PUBLIC_ENV 환경변수가 제대로 설정되어 있지 않습니다. ${next_env}는 유효하지 않은 값입니다.`);
+            throw new Error(
+              `NEXT_PUBLIC_ENV 환경변수가 제대로 설정되어 있지 않습니다. ${next_env}는 유효하지 않은 값입니다.`,
+            );
           })();
 
 export const PoPoAxios = axios.create({
@@ -30,7 +34,9 @@ export const paxiApiUrl =
       : next_env === 'local'
         ? 'https://localhost:4100'
         : (() => {
-            throw new Error(`NEXT_PUBLIC_ENV 환경변수가 제대로 설정되어 있지 않습니다. ${next_env}는 유효하지 않은 값입니다.`);
+            throw new Error(
+              `NEXT_PUBLIC_ENV 환경변수가 제대로 설정되어 있지 않습니다. ${next_env}는 유효하지 않은 값입니다.`,
+            );
           })();
 
 export const PaxiAxios = axios.create({
