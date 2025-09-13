08388a4131af11a6a837648ac3bd90c169ed2eac
Gwanho Kim <khkim6040@gmail.com>
2025-09-13 22:19:10 +0900

    Update: Explicitly fail when env is not provided

--- utils/axios.instance.js
+++ utils/axios.instance.js
@@ -2,6 +2,10 @@ import axios from 'axios';
 
 const next_env = process.env.NEXT_PUBLIC_ENV;
 
+if (!next_env) {
+  throw new Error('.env 파일에 NEXT_PUBLIC_ENV 환경변수가 설정되어 있지 않습니다.');
+}
+
 export const popoApiUrl =
   next_env === 'prod'
     ? 'https://api.popo.poapper.club'
@@ -9,7 +13,9 @@ export const popoApiUrl =
       ? 'https://api.popo-dev.poapper.club'
       : next_env === 'local'
         ? 'https://localhost:4000'
-        : new Error('NEXT_PUBLIC_ENV is not set or invalid');
+        : (() => {
+            throw new Error(`NEXT_PUBLIC_ENV 환경변수가 제대로 설정되어 있지 않습니다. ${next_env}는 유효하지 않은 값입니다.`);
+          })();
 
 export const PoPoAxios = axios.create({
   baseURL: popoApiUrl,
@@ -23,7 +29,9 @@ export const paxiApiUrl =
       ? 'https://api.paxi.popo-dev.poapper.club'
       : next_env === 'local'
         ? 'https://localhost:4100'
-        : new Error('NEXT_PUBLIC_PAXI_ENV is not set or invalid');
+        : (() => {
+            throw new Error(`NEXT_PUBLIC_ENV 환경변수가 제대로 설정되어 있지 않습니다. ${next_env}는 유효하지 않은 값입니다.`);
+          })();
 
 export const PaxiAxios = axios.create({
   baseURL: paxiApiUrl,
